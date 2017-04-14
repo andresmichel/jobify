d2d0588358a71b837d62a023b56ca23ff215f260
Andres Michel <andresmichelgonzalez@gmail.com>
2017-04-13 22:11:21 -0700

    making progress

--- resources/views/public/company/show.blade.php
+++ resources/views/public/company/show.blade.php
@@ -24,18 +24,20 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-sm-8 offset-sm-2">
-                <h3 class="mb-4">Ofertas de trabajo</h3>
-                <div class="card mb-5">
-                    <ul class="list-group list-group-flush">
-                        @foreach ($jobs as $job)
-                            @include('partials.job-item')
-                        @endforeach
-                    </ul>
+        @if (count($jobs))
+            <div class="row">
+                <div class="col-sm-8 offset-sm-2">
+                    <h3 class="mb-4">Ofertas de trabajo</h3>
+                    <div class="card mb-5">
+                        <ul class="list-group list-group-flush">
+                            @foreach ($jobs as $job)
+                                @include('partials.job-item')
+                            @endforeach
+                        </ul>
+                    </div>
+                    {{ $jobs->links('vendor.pagination.bootstrap-4') }}
                 </div>
-                {{ $jobs->links('vendor.pagination.bootstrap-4') }}
             </div>
-        </div>
+        @endif
     </div>
 @endsection
